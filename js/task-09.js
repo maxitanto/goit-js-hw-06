9d38e0fdffda4f9e16899f6ae02fae4a058fb026
Maksym Baldyniuk <frundzik1989@gmail.com>
2023-05-04 18:27:22 +0300

    Add task - 09

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,18 @@
+const btnRef = document.querySelector('.change-color');
+const spanRef = document.querySelector('.color');
+const bodyRef = document.querySelector('body');
+
+btnRef.addEventListener('click', onChangeColor);
+
+function onChangeColor() {
+  spanRef.textContent = `- ${getRandomHexColor()}`
+
+  bodyRef.style.backgroundColor = getRandomHexColor();
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
